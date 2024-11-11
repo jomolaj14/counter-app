db2d0228358ae4a9dab94e901bbdfe682eee8a30
jomolaj14 <ajjomol2004@gmail.com>
2024-11-11 10:16:59 +0530

    counter app

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+ name:'counter',
+ initialState:{
+    count : 0
+ },
+ reducers:{
+    increment: (state)=>{
+        state.count++
+    },
+    decrement: (state)=>{
+      state.count--
+    },
+    reset : (state)=>{
+      return {...state,count:0}
+    },
+    incrementByAmount : (state,actionBycounter)=>{
+    state.count += actionBycounter.payload
+    }
+   
+      
+
+    
+    
+ }
+})
+export const {increment,decrement,reset,incrementByAmount} =counterSlice.actions
+export default counterSlice.reducer
